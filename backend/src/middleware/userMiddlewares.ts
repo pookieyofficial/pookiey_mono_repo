c622e539fe76f1d6cbf2683a4d0b35c64978f8de
Piyush Choudhary <contactpiyushhere@gmail.com>
2025-09-13 01:43:41 +0530

    FIX: minor fixes

--- backend/src/middleware/userMiddlewares.ts
+++ backend/src/middleware/userMiddlewares.ts
@@ -39,7 +39,6 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
             user: user
         } as any;
         
-        console.log({ reqUser: req.user, userExists: !!user });
         next();
     }
     catch (error) {
