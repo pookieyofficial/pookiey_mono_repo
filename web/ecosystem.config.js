8b8fad5026513bacf908297179c9ebaad65e3850
thedevpiyush <contactpiyushhere@gmail.com>
2025-12-23 00:51:45 +0530

    fixed ecosystem config for web

--- web/ecosystem.config.js
+++ web/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "web",
-        cwd: "/var/www/your-repo/web",
+        cwd: "/var/www/pookiey_mono_repo/web",
         script: "node_modules/next/dist/bin/next",
         args: "start -p 3000",
         env: {
