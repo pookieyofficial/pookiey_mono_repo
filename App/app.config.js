286561e4406ad2e042f471c4f7e0fcb42e2e2be9
Piyush Choudhary <contactpiyushhere@gmail.com>
2026-02-01 16:41:14 +0530

    version incremented

--- App/app.config.js
+++ App/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Pookiey",
     slug: "pookieycom",
-    version: "1.0.0",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "pookiey",
